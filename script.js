6c8dea92b461ddc5cf737fd0d95def3dadb448de
Alexandra Neagoe <77905880+alexa396@users.noreply.github.com>
2024-10-12 13:03:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const months = [
   "Dec",
 ];
 // API key for OpenWeatherMap API
-const API_KEY = "49cc8c821cd2aff9af04c9f98c36eb74";
+const API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 let hourIn12HourFormat;
 function updateCurrentTime(timezone) {
